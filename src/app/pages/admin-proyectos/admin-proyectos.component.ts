554cc37f6ca87e1eebdbaf992ee6a25fcb59baed
jjperez26 <jjperez26@misena.edu.co>
2023-09-01 15:02:58 -0500

    Actualizacion 01 septiembre

--- src/app/pages/admin-proyectos/admin-proyectos.component.ts
+++ src/app/pages/admin-proyectos/admin-proyectos.component.ts
@@ -6,8 +6,11 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {projectname: 'Andres', projectstartdate: "01/01/2023", projectenddate:"01/01/2023"},
-  {projectname: 'Andres', projectstartdate: "01/01/2023", projectenddate:"01/01/2023"}
+  {projectname: 'Help Desk', projectstartdate: "01/11/2023", projectenddate:"15/01/2024"},
+  {projectname: 'Ayuditas', projectstartdate: "01/07/2023", projectenddate:"20/09/2023"},
+  {projectname: 'Manos Creaciones', projectstartdate: "01/06/2023", projectenddate:"30/08/2023"},
+  {projectname: 'Mascotas Felices', projectstartdate: "05/08/2023", projectenddate:"12/11/2023"},
+  {projectname: 'Sorro con S', projectstartdate: "27/07/2023", projectenddate:"18/09/2023"},
 ];
 
 @Component({
